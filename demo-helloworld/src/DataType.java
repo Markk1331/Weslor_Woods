bbcca1b631b4cd3706f23c605641b59563ef0976
Markk1331 <markli1231991@gmail.com>
2023-07-11 17:19:04 +0800

    day 2 exercise

---
+++ demo-helloworld/src/DataType.java
@@ -0,0 +1,82 @@
+public class DataType {
+  public static void main(String[]args){
+    //number, text
+
+    int num = 3; //declare a variable num as integer
+
+    //System.out.println(num);
+    //System.out.println(3);
+    //System.out.println("3");
+
+    //string testing
+
+    //String str = "hello..."; //declare a string with capital "S"
+   // System.out.println(str);
+
+    //double f = 1.03;
+
+    //System.out.println(f);
+    
+    //float testing
+    String abc = "abc";
+    //float p = (float)1.2321;
+    //float pp = 1.2222f;
+
+    //System.out.println(abc);
+    //System.out.println(p);
+    //System.out.println(pp);
+
+    //byte, long testing
+
+    byte w = 127;
+    //System.out.println(w);
+
+    long ak47 = 10000000000000L;
+    //System.out.println(ak47);
+
+    //boolean
+
+    boolean Hihi = false;
+
+    //System.out.println(Hihi);
+
+    //char
+    char baaabbbccc = 'A';
+    char female = 'F';
+    char akkk = 66;
+
+    //System.out.println(akkk);
+
+
+
+    //System.out.println(female);
+
+    short uou = 30000;
+
+    //System.out.println(uou);
+
+    String I_love_you = "I" + "love" + "you";
+
+    System.out.println(I_love_you);
+
+    int string1 = 5;
+    int string2 = 10;
+
+    System.out.println(100<string1 + string2);
+
+    int ten = 5 + 5;
+    System.out.println(ten);
+
+    String aaa = "54352";
+    char bbb = aaa.charAt(1);
+
+    System.out.println(bbb);
+
+    String peo = "a" + 'b' + 'c';
+    System.out.println(peo);
+
+    String a = "A";
+    System.out.println(a);
+
+  }
+}
